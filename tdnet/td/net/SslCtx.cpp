0486ddf085904f4a09973d1350c9103507459c99
David Osipov <personal@david-osipov.vision>
2026-04-21 20:09:00 +0400

    ssl: avoid unbounded strlen on CSlice cert path

--- tdnet/td/net/SslCtx.cpp
+++ tdnet/td/net/SslCtx.cpp
@@ -342,10 +342,11 @@ Result<SslCtxPtr> do_create_ssl_ctx(CSlice cert_file, SslCtx::VerifyPeer verify_
   } else {
     // Fail closed on embedded NUL to prevent path truncation ambiguities,
     // where "trusted.pem\0suffix" could be interpreted as "trusted.pem".
-    if (cert_file.size() != std::strlen(cert_file.c_str())) {
+    if (std::memchr(cert_file.begin(), '\0', cert_file.size()) != nullptr) {
       return Status::Error("Explicit certificate path contains an embedded NUL byte");
     }
-    if (SSL_CTX_load_verify_locations(ssl_ctx, cert_file.c_str(), nullptr) == 0) {
+    string cert_file_str = cert_file.str();
+    if (SSL_CTX_load_verify_locations(ssl_ctx, cert_file_str.c_str(), nullptr) == 0) {
       return create_openssl_error(-8, "Failed to set custom certificate file");
     }
   }
